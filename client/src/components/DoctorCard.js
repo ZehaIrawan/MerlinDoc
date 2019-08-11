9371ebca9558f0a72e932061de2f5826b0082867
Zeha Irawan <jangkaringbumi@gmail.com>
2019-08-11 21:01:35 +0700

    Add appropriate components, json passed as props

--- client/src/components/DoctorCard.js
+++ client/src/components/DoctorCard.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const DoctorCard = () => {
+const DoctorCard = props => {
+  const { name, profileImg, address, specialties, phoneNumber } = props;
   return (
-    <div>
-      
+    <div className="doctorCardContainer">
+      <img src={profileImg} alt={profileImg} />
+      <h2>{name}</h2>
+      <p>{specialties}</p>
+      <h3>Address</h3>
+      <p>{address}</p>
+      <button>View Profile</button>
+      <h4>Call {phoneNumber}</h4>
+      <div>
+        <a href="http://google.com">View on map</a>
+      </div>
+      <br />
     </div>
-  )
-}
+  );
+};
 
-export default DoctorCard
+export default DoctorCard;
--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import data from '../data/data';
+import DoctorCard from '../components/DoctorCard';
 
 const Search = () => {
-  console.log(data[0].data[0].practices[0]);
+  const [doctor, setDoctor] = useState(data);
   return (
     <div>
-      Search
-      {data[0].data.map(e => (
-        <div key={e.profile.slug}>
-          <p>
-            {`${e.profile.first_name} ${e.profile.last_name}, ${
-              e.profile.title
-            }`}
-          </p>
-          <p>{`${e.practices[0].visit_address.street}, ${
-            e.practices[0].visit_address.city
-          }, ${e.practices[0].visit_address.state_long} `}</p>
+      <h1 style={{ color: 'blue',marginLeft:'25%' }}>Search</h1>
+      {data[0].data.map(doctor => (
+        <DoctorCard
+          key={doctor.profile.slug}
+          name={`${doctor.profile.first_name} ${doctor.profile.last_name}, ${
+            doctor.profile.title
+          }`}
+          profileImg={doctor.profile.image_url}
+          address={`${doctor.practices[0].visit_address.street}, ${
+            doctor.practices[0].visit_address.city
+          }, ${doctor.practices[0].visit_address.state_long} `}
+          specialties={doctor.specialties.map(s => (
+            `${ s.actor} ,`
+           ))}
+          phoneNumber={doctor.practices[0].phones[0].number}
 
-          <h1>Call {e.practices[0].phones[0].number}</h1>
-
-          <a href="google.com">View on map</a>
-        </div>
+        />
       ))}
     </div>
   );
