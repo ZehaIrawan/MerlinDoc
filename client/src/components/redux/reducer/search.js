cb896b675a144cd1b568b55e446f8970a09d1dbd
Zeha Irawan <jangkaringbumi@gmail.com>
2019-08-23 22:35:42 +0700

    create query reducer

--- client/src/components/redux/reducer/search.js
+++ client/src/components/redux/reducer/search.js
@@ -1,13 +1,20 @@
-const query = (state = {}, action) => {
-  const { type, query } = action;
-
-  switch (type) {
-    case 'ADD_URL': {
-      console.log('remove');
-    }
+const initialState = {
+  query:{
+    location: 'NY',
+    sympton: 'Flu'
+  }
+};
+function query(state = initialState, action) {
+  switch (action.type) {
+    case 'INC':
+      return { ...state, count: state.count + 1 };
+    case 'DEC':
+      return { ...state, count: state.count - 1 };
+    case 'RESET':
+      return { ...state, count: 0 };
     default:
       return state;
   }
-};
+}
 
 export default query;
